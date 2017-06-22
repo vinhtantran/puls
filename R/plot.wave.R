fc0c29143b1331d36ec27bb4495584ddb61c35aa
Tan Tran <vinhtantran@gmail.com>
2017-06-22 12:19:08 -0600

    update medoid

--- R/plot.wave.R
+++ R/plot.wave.R
@@ -1,5 +1,13 @@
-plot.wave <- function(fd, puls, member = T, medians = T, ...) {
+plot.wave <- function(fd, puls, member = T, medoids = T, lty = 3, ...) {
   if (member) {
-    plot(fd, col=puls$Membership, ...)
-  } else plot(fd, ...)
+    plot(fd, col=puls$Membership, lty, ...)
+  } else plot(fd, col="gray", ...)
+
+  if (medoids) {
+    member <- unique(puls$Membership)
+    for (i in member) {
+      lines(1:dim(fd$argvals)[1], fd$y[,puls$frame$medoid[i]], type = "l", col=i, lwd=2)
+    }
+
+  }
 }
