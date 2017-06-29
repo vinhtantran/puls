5f7cae5d1258dfb21cee6b70ba77175811d0f7a4
Tan Tran <vinhtantran@gmail.com>
2017-06-29 01:51:11 -0600

    fix

--- R/plot.wave.R
+++ R/plot.wave.R
@@ -12,6 +12,10 @@ plot.wave <- function(fd, puls, member = T, medoids = T, ...) {
     lwd.lst <- rep(1, length(puls$Membership))
     lwd.lst[meds] <- 3
 
-    plot(fd$fd, col=puls$Membership - min(puls$Membership) + 1, lty=lty.lst, lwd=lwd.lst)
+    # A trick to change color starting from 1
+    m <- as.factor(puls$Membership)
+    levels(m) <- 1:length(m)
+    m <- as.numeric(m)
+    plot(fd$fd, col=m, lty=lty.lst, lwd=lwd.lst)
   }
 }
