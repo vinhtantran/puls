662206e26c8da5e82551fd1373c3a3ca06f800eb
Tan Tran <vinhtantran@gmail.com>
2017-06-29 00:26:08 -0600

    fix membership variable

--- R/plot.wave.R
+++ R/plot.wave.R
@@ -7,14 +7,14 @@ plot.wave <- function(fd, puls, member = T, medoids = T, ...) {
 
   if (medoids) {
     lty.lst <- rep(3, length(puls$Membership))
-    lty.lst[puls$frame$medoid[member]] <- 1
+    lty.lst[puls$frame$medoid[mbs]] <- 1
 
     lwd.lst <- rep(1, length(puls$Membership))
-    lwd.lst[puls$frame$medoid[member]] <- 3
+    lwd.lst[puls$frame$medoid[mbs]] <- 3
 
     plot(fd$fd, col=puls$Membership - min(puls$Membership) + 1, lty=lty.lst, lwd=lwd.lst)
 
-    for (i in member) {
+    for (i in mbs) {
       lines(1:365, fd$y[,puls$frame$medoid[i]], type = "l", col=i, lwd=2)
     }
 
