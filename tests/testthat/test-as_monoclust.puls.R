d2a4d556bcf31f630e305a7ed26b88c6a7c21114
Tan Tran <vinhtantran@gmail.com>
2021-02-14 18:18:14 -0500

    remove a test that should happen at monoClust

--- tests/testthat/test-as_monoclust.puls.R
+++ tests/testthat/test-as_monoclust.puls.R
@@ -1,10 +1,4 @@
 test_that("print error when coercing wrong PULS class to MonoClust class", {
-  testthat::expect_warning({
-    a <- list(test = 1:3)
-    as_MonoClust(a)
-  },
-  "as_MonoClust does not know how to handle object of class list.")
-
   testthat::expect_error({
     b <- list(test = 1:3, y = 5)
     class(b) <- "PULS"
